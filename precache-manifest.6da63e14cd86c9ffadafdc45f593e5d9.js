5c745413bbf6a5eb4a62bed5cf4ea6d6406f69ef
Pasecinic Nichita <nichitastrix@gmail.com>
2021-11-20 12:24:19 +0200

    Updates

---
+++ precache-manifest.6da63e14cd86c9ffadafdc45f593e5d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33dc70a895c5a6569e4396a69d056a02",
+    "url": "/react-translate-plugin/index.html"
+  },
+  {
+    "revision": "4919089ad65948de6f5c",
+    "url": "/react-translate-plugin/static/css/2.740a6a30.chunk.css"
+  },
+  {
+    "revision": "4919089ad65948de6f5c",
+    "url": "/react-translate-plugin/static/js/2.de1e41cd.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/react-translate-plugin/static/js/2.de1e41cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9191a397db94b8c98524",
+    "url": "/react-translate-plugin/static/js/main.2f89da6e.chunk.js"
+  },
+  {
+    "revision": "cde166d80440bc5b5b78",
+    "url": "/react-translate-plugin/static/js/runtime-main.3a44f70d.js"
+  }
+]);
